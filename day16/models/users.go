ba3ca00b75ed10df46f3b41a3f942950d12cb01d
arun fung <arunfung@gmail.com>
2022-01-23 19:58:58 +0800

    day16-02

--- day16/models/users.go
+++ day16/models/users.go
@@ -9,6 +9,7 @@ type Users struct {
 	Username string `orm:"column(name)"`
 	Password string
 	Email string
+	Role *UserRole `orm:"rel(fk)"` // 表示的是 users与role ; 一对一 => rel(fk) 定义的时候 -》查找Users表中的 role_id => 根据结构体的属性 跟上 id  正向 -》 从自身查找
 }
 
 type UserRole struct {
@@ -16,7 +17,7 @@ type UserRole struct {
 	Name  string `orm:"column(name)"`
 	Desc  string `orm:"column(desc)"`
 	// 反向一对多 beego-orm 根据 Users结构体查找 含有 `orm:"rel(fk)"`的属性，确定关联关系
-	//Users []*Users `orm:"reverse(many)"`
+	Users []*Users `orm:"reverse(many)"`
 }
 
 func init() {
@@ -28,6 +29,15 @@ func FindUserByName(name string) (*Users,error) {
 	o := orm.NewOrm()
 
 	user := &Users{}
-	o.QueryTable("Users").Filter("name",name).One(user)
+	o.QueryTable("Users").Filter("name",name).RelatedSel().One(user)
 	return user,nil
+}
+
+func FindRoleById(id int) (*UserRole,error) {
+	o := orm.NewOrm()
+
+	role := &UserRole{}
+	o.QueryTable("UserRole").Filter("id",id).One(role)
+	o.LoadRelated(role, "Users")
+	return role,nil
 }
---
+++ day16/models/users_test.go
@@ -0,0 +1,86 @@
+package models
+
+import (
+	"fmt"
+	"github.com/beego/beego/v2/client/orm"
+	_ "github.com/go-sql-driver/mysql" // import your used driver
+	"reflect"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	orm.RegisterDataBase("default", "mysql",
+		fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4", "root","root123456","127.0.0.1",3306,"go"))
+	orm.Debug = true
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	m.Run()
+}
+
+func Test_findUserByName(t *testing.T) {
+	type args struct {
+		name string
+	}
+	var tests = []struct {
+		name    string
+		args    args
+		want    *Users
+		wantErr bool
+	}{
+		{
+			name: "get name",
+			args: args{name: "arun"},
+			want: &Users{
+				Id:       1,
+				Username: "arun",
+				Password: "123456",
+				Email:    "arun@gmail.com",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FindUserByName(tt.args.name)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("findUserByName() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findUserByName() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFindRoleById(t *testing.T) {
+	type args struct {
+		id int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *UserRole
+		wantErr bool
+	}{
+		{
+			name: "get role",
+			args: args{id: 1},
+			want: &UserRole{
+				Id: 1,
+				Name: "管理员",
+				Desc: "管理员",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FindRoleById(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FindRoleById() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FindRoleById() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
